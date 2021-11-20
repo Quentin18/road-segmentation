291add4449225014f800d71fa9ebeec575787060
Quentin18 <quentindeschamps18@orange.fr>
2021-11-20 19:42:11 +0100

    add nets.py

---
+++ src/nets.py
@@ -0,0 +1,17 @@
+"""
+Neural networks.
+"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class UNet(nn.Module):
+    """
+    UNet neural network.
+
+    TODO to implement
+    """
+    def __init__(self) -> None:
+        super().__init__()
+        raise NotImplementedError
