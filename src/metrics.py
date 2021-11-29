6ffe55847bfcc018f266cada854961c43538fc27
Quentin18 <quentindeschamps18@orange.fr>
2021-11-29 17:53:48 +0100

    update metrics.py

--- src/metrics.py
+++ src/metrics.py
@@ -7,7 +7,7 @@ from sklearn.metrics import accuracy_score, f1_score
 
 def accuracy_score_tensors(
     target: torch.Tensor,
-    output: torch.Tensor
+    output: torch.Tensor,
 ) -> float:
     """Accuracy classification score from tensors.
 
@@ -18,14 +18,14 @@ def accuracy_score_tensors(
     Returns:
         float: accuracy score between 0 and 1.
     """
-    target = target.flatten().detach().numpy().astype(int)
-    output = output.flatten().detach().numpy().astype(int)
-    return accuracy_score(target, output, normalize=True)
+    target_flatten = torch.flatten(target)
+    output_flatten = torch.flatten(output)
+    return accuracy_score(target_flatten, output_flatten, normalize=True)
 
 
 def f1_score_tensors(
     target: torch.Tensor,
-    output: torch.Tensor
+    output: torch.Tensor,
 ) -> float:
     """F1 score from tensors.
 
@@ -36,6 +36,6 @@ def f1_score_tensors(
     Returns:
         float: f1 score between 0 and 1.
     """
-    target = target.flatten().detach().numpy().astype(int)
-    output = output.flatten().detach().numpy().astype(int)
-    return f1_score(target, output)
+    target_flatten = torch.flatten(target)
+    output_flatten = torch.flatten(output)
+    return f1_score(target_flatten, output_flatten)
