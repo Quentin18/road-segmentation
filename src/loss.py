8aa8e06df94b7d591677091fcd91403f371da8dd
Quentin18 <quentindeschamps18@orange.fr>
2021-12-18 18:12:05 +0100

    update loss.py

--- src/loss.py
+++ src/loss.py
@@ -1,23 +1,29 @@
 """
 Custom loss functions.
+
+Source:
+https://www.kaggle.com/bigironsphere/loss-function-library-keras-pytorch
 """
 import torch.nn as nn
 
 
 class DiceLoss(nn.Module):
     """
-    Dice loss.
+    Criterion that computes Sørensen-Dice Coefficient loss.
+
+    https://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient
     """
     def __init__(self):
-        super(DiceLoss, self).__init__()
+        super().__init__()
         self.smooth = 1.0
 
-    def forward(self, y_pred, y_true):
-        assert y_pred.size() == y_true.size()
-        y_pred = y_pred[:, 0].contiguous().view(-1)
-        y_true = y_true[:, 0].contiguous().view(-1)
-        intersection = (y_pred * y_true).sum()
-        dsc = (2. * intersection + self.smooth) / (
-            y_pred.sum() + y_true.sum() + self.smooth
-        )
-        return 1. - dsc
+    def forward(self, input, target):
+        # Flatten label and prediction tensors
+        input = input.view(-1)
+        target = target.view(-1)
+
+        intersection = (input * target).sum()
+        dice = (2. * intersection + self.smooth) / (
+            input.sum() + target.sum() + self.smooth)
+
+        return 1 - dice
