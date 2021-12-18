ad2c948d008dd97845e9fdcf1d2df8e12c564ef1
Quentin18 <quentindeschamps18@orange.fr>
2021-12-18 18:14:23 +0100

    update data_augmentation.py

--- src/data_augmentation.py
+++ src/data_augmentation.py
@@ -6,23 +6,26 @@ import shutil
 
 import torchvision.transforms.functional as TF
 from PIL import Image
-from torchvision.transforms import FiveCrop, Resize
+from torchvision.transforms import Resize, TenCrop
 from tqdm import trange
 
 from src.path import (DATA_TRAIN_AUG_GT_PATH, DATA_TRAIN_AUG_IMG_PATH,
                       DATA_TRAIN_AUG_PATH, DATA_TRAIN_GT_PATH,
                       DATA_TRAIN_IMG_PATH)
 
+# Size of the base images
+BASE_IMG_SIZE = (400, 400)
+
 # Size of the new images
-AUG_IMG_SIZE = 256
+AUG_IMG_SIZE = (256, 256)
 
 # Angles for rotations
-ANGLES = [-90, -45, 45, 90]
+ANGLES = [45 * (i + 1) for i in range(7)]
 
 
 class MultipleRotationCrop:
     """Rotate the image by the given angles and center crop."""
-    def __init__(self, angles: list, size: int):
+    def __init__(self, angles: list, size: tuple):
         self.angles = angles
         self.size = size
 
@@ -33,13 +36,29 @@ class MultipleRotationCrop:
         ]
 
 
+def save_aug_img_mask(image: Image, mask: Image, num: int) -> None:
+    """Saves an image and a mask in the augmented directories.
+
+    Args:
+        image (Image): image.
+        mask (Image): mask.
+        num (int): number of the image/mask.
+    """
+    filename = f'satImage_{num:04d}.png'
+    image_path = os.path.join(DATA_TRAIN_AUG_IMG_PATH, filename)
+    mask_path = os.path.join(DATA_TRAIN_AUG_GT_PATH, filename)
+    image.save(image_path)
+    mask.save(mask_path)
+
+
 def create_augmented_dataset(replace: bool = False) -> None:
     """Creates the augmented dataset.
 
-    It contains 1000 images of size 256x256:
-    - Resized images from train data (100)
-    - Cropped images into four corners and the central crop (5x100)
-    - Rotated and cropped images (4x100)
+    It contains 1800 images of size 256x256:
+    - Resize images from train data (100)
+    - Crop images into four corners and the central crop plus the flipped
+    version of these (100x10)
+    - Rotate images and center crop (100x7)
 
     Args:
         replace (bool, optional): True to replace images if already exist.
@@ -61,16 +80,15 @@ def create_augmented_dataset(replace: bool = False) -> None:
     masks_names = sorted(os.listdir(DATA_TRAIN_GT_PATH))
 
     # Define transforms
-    size = (AUG_IMG_SIZE, AUG_IMG_SIZE)
-    resize = Resize(size)
-    five_crop = FiveCrop(size)
-    multiple_rotate_crop = MultipleRotationCrop(ANGLES, size)
+    resize = Resize(size=AUG_IMG_SIZE)
+    ten_crop = TenCrop(size=AUG_IMG_SIZE)
+    multiple_rotation_crop = MultipleRotationCrop(
+        angles=ANGLES,
+        size=AUG_IMG_SIZE,
+    )
 
-    # Number of crops by image
-    nb_crop = 5
-
-    # Number of rotations by image
-    nb_rot = len(ANGLES)
+    # Counter for the number of the image/mask
+    num = 1
 
     # Create augmented dataset
     with trange(len(images_names), unit='image') as t:
@@ -94,49 +112,24 @@ def create_augmented_dataset(replace: bool = False) -> None:
             mask_resized = resize(mask)
 
             # Save resized images
-            image_resized_path = os.path.join(
-                DATA_TRAIN_AUG_IMG_PATH, image_name
-            )
-            mask_resized_path = os.path.join(
-                DATA_TRAIN_AUG_GT_PATH, mask_name
-            )
-            image_resized.save(image_resized_path)
-            mask_resized.save(mask_resized_path)
+            save_aug_img_mask(image_resized, mask_resized, num)
+            num += 1
 
-            # 2. Crop the images into four corners and the central crop
-            images_crop = five_crop(image)
-            masks_crop = five_crop(mask)
+            # 2. Crop images into four corners and the central crop plus the
+            # flipped version of these
+            images_cropped = ten_crop(image)
+            mask_cropped = ten_crop(mask)
 
             # Save cropped images
-            for k in range(nb_crop):
-                image_crop = images_crop[k]
-                mask_crop = masks_crop[k]
+            for i, m in zip(images_cropped, mask_cropped):
+                save_aug_img_mask(i, m, num)
+                num += 1
 
-                filename = f'satImage_crop{i * nb_crop + k}.png'
-                image_crop_path = os.path.join(
-                    DATA_TRAIN_AUG_IMG_PATH, filename
-                )
-                mask_crop_path = os.path.join(
-                    DATA_TRAIN_AUG_GT_PATH, filename
-                )
-                image_crop.save(image_crop_path)
-                mask_crop.save(mask_crop_path)
-
-            # 3. Rotate and crop images
-            images_rot = multiple_rotate_crop(image)
-            masks_rot = multiple_rotate_crop(mask)
+            # 3. Rotate images and center crop
+            images_rotated = multiple_rotation_crop(image)
+            mask_rotated = multiple_rotation_crop(mask)
 
             # Save rotated images
-            for k in range(nb_rot):
-                image_rot = images_rot[k]
-                mask_rot = masks_rot[k]
-
-                filename = f'satImage_rot{i * nb_rot + k}.png'
-                image_rot_path = os.path.join(
-                    DATA_TRAIN_AUG_IMG_PATH, filename
-                )
-                mask_rot_path = os.path.join(
-                    DATA_TRAIN_AUG_GT_PATH, filename
-                )
-                image_rot.save(image_rot_path)
-                mask_rot.save(mask_rot_path)
+            for i, m in zip(images_rotated, mask_rotated):
+                save_aug_img_mask(i, m, num)
+                num += 1
